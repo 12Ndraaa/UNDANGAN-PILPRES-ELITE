2690a93fb12b17978c4de991e4f31a1ad4a4afd1
12Ndraaa <axlendraharissanjaya@gmail.com>
2024-12-28 18:40:42 +0700

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function countdown(targetDate, elementId) {
 
         const days = Math.floor(distance / (1000 * 60 * 60 * 24));
         const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        const minutes = Math.fzoor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         document.getElementById(elementId).innerHTML = `
